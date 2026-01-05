3798799559c623e03c2985fcf058d0d84cad2074
M.Vihtória <mariavitoriasilvaaraujo326@gmail.com>
2026-01-04 23:10:13 -0300

    telas de user stake

--- Frontend/src/view/App.tsx
+++ Frontend/src/view/App.tsx
@@ -5,11 +5,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaDeLogin from './logarCadastrar/TelaDeLogin';
 import TelaDeCadastro from './logarCadastrar/TelaDeCadastro';
 import TelaInicial from './TelaInicial';
-
+import Perfil from '../view/user/perfil';
+import TrocarSenha from '../view/user/trocarSenha';
+import Notificacoes from '../view/user/notificacoes';
+import Informacoes from '../view/user/informacao';
+ 
 export type RootStackParamList = {
   Login: undefined;
   TelaInicial: { nome: string };
   CadastrarUsuário: { nome: string };
+  Perfil: undefined;
+  TrocarSenha: undefined;
+  Notificacoes: undefined;
+  Informacoes: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -25,5 +33,16 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+export function UserStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Perfil" component={Perfil} />
+      <Stack.Screen name="TrocarSenha" component={TrocarSenha} />
+      <Stack.Screen name="Notificacoes" component={Notificacoes} />
+      <Stack.Screen name="Informacoes" component={Informacoes} />
+    </Stack.Navigator>
+  )
+}
 
 export default App;
